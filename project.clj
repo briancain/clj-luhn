d58b9dea99476d88a0556fad83e43315c665c79c
Brian Cain <brianccain@gmail.com>
2015-01-01 17:04:50 -0800

    Update project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-luhn "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-luhn "0.1.0"
+  :description "A clojure library to validate credit card numbers using the luhn algorithm"
+  :url "http://github.com/briancain/clj-luhn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
